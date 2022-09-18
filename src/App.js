e0c243fe71ed893bb22045ab2fed0aa9a815b35a
Arthur Rodrigues <art.costa.2005@gmail.com>
2022-09-18 00:38:55 -0300

    add

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import './App.css';
-import {getDex} from './api/pokeapi';
+import {getDex, getID, getSprite} from './api/pokeapi';
 
 function App() {
-  const rawContent = getDex(0,151).then(data => data.results);
+  const [counter, setCounter] = React.useState(0);
   const [content, setContent] = React.useState(null);
-
-
+  
+  const incrementCounter = () => {
+    setCounter(counter + 5);
+  };
+  const decrementCounter = () => {
+    if (counter > 0) {
+      setCounter(counter - 5);
+    }
+  };
+  
   React.useEffect(() => {
-    rawContent.then(data => data.map((item, index) => {
-      return `${index + 1} - ${item.name}`;
-    })).then(data => {
-      setContent(data);
-    })
-  }, [])
+    const rawContent = getDex(counter, 5).then(data => data.results);
+    rawContent.then(data => {setContent(data)});
+  }, [counter])
 
-  if (content === null) return 'Nothing to be exhibit yet';
+  if (content === null) return 'Loading...';
 
   return (
     <div className="App">
       <div>
+        <div>
+          <button onClick={incrementCounter}>Próx.</button>
+          <button onClick={decrementCounter}>Ante.</button>
+        </div>
         {content.map(item => {
-          return <div>{item}</div>
+          return (
+            <>
+              <div>{getID(item.url)} - {item.name}</div>
+              <img className='sprite' src={getSprite(item.name)}/>
+            </>
+          )
         })}
       </div>
     </div>
--- src/api/pokeapi.js
+++ src/api/pokeapi.js
@@ -1,4 +1,3 @@
-
 export const getDex = (offset, limit) => {
   const baseLink = 'https://pokeapi.co/api/v2/pokemon-species/';
   let apiLink;
@@ -12,3 +11,33 @@ export const getDex = (offset, limit) => {
   
   return req;
 };
+
+export const getSprite = (name) => {
+  const nonRegularNames = {
+    deoxys: 'deoxys-normal',
+    giratina : 'giratina-altered',
+    shaymin : 'shaymin-land',
+    oricorio : 'oricorio-baile',
+    lycanroc : 'lycanroc-midday',
+    wishiwashi : 'wishiwashi-solo',
+    eiscue : 'eiscue-ice',
+    enamorus : 'enamorus-incarnate'
+  }
+
+  if (nonRegularNames[name] !== undefined) {
+    name = nonRegularNames[name];
+  }
+  const link = `https://img.pokemondb.net/artwork/large/${name}.jpg`;
+  
+  return link;
+}
+
+export const getID = (link) => {
+  let id = '';
+  let i;
+  for (i = 42; i < link.length - 1; i++) {
+    id += link[i];
+  }
+  
+  return id;
+}
