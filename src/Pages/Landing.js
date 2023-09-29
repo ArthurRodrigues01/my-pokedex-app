f8f8fb509749963388d39e6c5ae3273dcbf877f9
Arthur Rodrigues <88101116+ArthurRodrigues01@users.noreply.github.com>
2023-09-29 19:27:42 -0300

    Delete src directory

--- src/Pages/Landing.js
+++
@@ -1,9 +0,0 @@
-const Landing = () => {
-  return (
-    <h1>
-      Pokedéx do balaco-baco
-    </h1>
-  );
-};
-
-export default Landing;
--- src/Pages/OverView.js
+++
@@ -1,9 +0,0 @@
-const OverView = () => {
-  return (
-    <h1>
-      List of pokemons should appear here
-    </h1>
-  );
-}
-
-export default OverView;
